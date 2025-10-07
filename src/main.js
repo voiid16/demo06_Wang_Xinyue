10f75bc96e77b9e8a77a89ed19aa586b797516a3
Ivyxw <wangxinyue0198@outlook.com>
2025-10-07 11:13:31 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+// If you have custom global styles, import them as well:
+import '/styles/style.css';
+
+function sayHello() {
+  // TODO: implement your logic here
+}
+document.addEventListener('DOMContentLoaded', sayHello);
