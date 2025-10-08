768196f59b407d4362f6853786afa2b78fc394fc
Xinyue <wangxinyue0198@outlook.com>
2025-10-07 20:28:00 -0700

    Finish review.html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 // If you have custom global styles, import them as well:
-import '/styles/style.css';
+import "/styles/style.css";
 
 function sayHello() {
   // TODO: implement your logic here
 }
-document.addEventListener('DOMContentLoaded', sayHello);
+document.addEventListener("DOMContentLoaded", sayHello);
